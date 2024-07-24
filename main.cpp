03a00bc9376ce0d644487bacc957c0e819dcc42b
laksudhre <laksudhre@gmail.com>
2024-07-24 15:51:24 -0700

    refactor

--- main.cpp
+++ main.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
 
-#define STR(text) #text
-#define DAY1 STR(monday)
-#define DAY2 STR(tusday)
-#define DAY3 STR(wednesday)
-#define DAY4 STR(thursday)
-#define DAY5 STR(friday)
-#define DAY6 STR(saturday)
-#define DAY7 STR(sunday)
+#define MONDAY 1
+#define TUESDAY 2
+#define WEDNESDAY 3
+#define THURSDAY 4
+#define FRIDAY 5
+#define SATURDAY 6
+#define SUNDAY 7
+#define MONDAY_NAME "Monday"
+#define TUESDAY_NAME "Tuesday"
+#define WEDNESDAY_NAME "Wednesday"
+#define THURSDAY_NAME "Thursday"
+#define FRIDAY_NAME "Friday"
+#define SATURDAY_NAME "Saturday"
+#define SUNDAY_NAME "Sunday"
+#define GET_NAME(index) index ## _NAME
 
 int main() {
     int input = 0;
     std::cout << "Enter a number: ";
     std::cin >> input;
     switch (input) {
-        case 1:
-            std::cout << DAY1 << std::endl;
+        case MONDAY:
+            std::cout << GET_NAME(MONDAY) << std::endl;
             break;
-        case 2:
-            std::cout << DAY2 << std::endl;
+        case TUESDAY:
+            std::cout << GET_NAME(TUESDAY) << std::endl;
             break;
-        case 3:
-            std::cout << DAY3 << std::endl;
+        case WEDNESDAY:
+            std::cout << GET_NAME(WEDNESDAY) << std::endl;
             break;
-        case 4:
-            std::cout << DAY4 << std::endl;
+        case THURSDAY:
+            std::cout << GET_NAME(THURSDAY) << std::endl;
             break;
-        case 5:
-            std::cout << DAY5 << std::endl;
+        case FRIDAY:
+            std::cout << GET_NAME(FRIDAY) << std::endl;
             break;
-        case 6:
-            std::cout << DAY6 << std::endl;
+        case SATURDAY:
+            std::cout << GET_NAME(SATURDAY) << std::endl;
             break;
-        case 7:
-            std::cout << DAY7 << std::endl;
+        case SUNDAY:
+            std::cout << GET_NAME(SUNDAY) << std::endl;
             break;
         default:
             std::cout << "Invalid input. Please enter a number between 1 and 7." << std::endl;
